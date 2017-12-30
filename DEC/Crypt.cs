74503056d125fcbb848874de838f9e7cd25de9e4
tktomaru <tk.tomaru@gmail.com>
2017-12-31 02:43:32 +0900

    開発／ベース作成

---
+++ DEC/Crypt.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography;
+using System.Globalization;
+
+namespace DEC
+{
+    public class Crypt
+    {
+
+        static byte[] zeroIV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        public byte[] HEX2Bytes(string hex)
+        {
+            if (hex.Length % 2 != 0)
+            {
+                throw new System.ArgumentException(System.String.Format(CultureInfo.InvariantCulture,
+                                                          "The binary key cannot have an odd number of digits: {0}", hex));
+            }
+
+            byte[] HexAsBytes = new byte[hex.Length / 2];
+            for (int index = 0; index < HexAsBytes.Length; index++)
+            {
+                string byteValue = hex.Substring(index * 2, 2);
+                HexAsBytes[index] = byte.Parse(byteValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+            }
+
+            return HexAsBytes;
+        }
+
+        public byte[] Encrypt(byte[] input, byte[] key)
+        {
+            var aesAlg = new AesManaged
+            {
+                KeySize = 128,
+                Key = key,
+                BlockSize = 128,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.Zeros,
+                IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            };
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+            return encryptor.TransformFinalBlock(input, 0, input.Length);
+        }
+
+ 
+        public byte[] Decrypt(byte[] input, byte[] key)
+        {
+            var aesAlg = new AesManaged
+            {
+                KeySize = 128,
+                Key = key,
+                BlockSize = 128,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.Zeros,
+                IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            };
+
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            return decryptor.TransformFinalBlock(input,0, input.Length);
+        }
+    }
+}
---
+++ DEC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DEC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Crypt wd = new Crypt();
+
+            //平文の文字列
+            //string enc_str = "30314030303030303030303030303030";
+            //暗号化した後の値
+            string dec_str = "AD780E95C370A53CC7C0E2D279848A77";
+            byte[] decdata = wd.HEX2Bytes(dec_str);
+
+            //鍵
+            string key_str = "07010203040506020701020304050602";
+            byte[] key = wd.HEX2Bytes(key_str);
+
+            var dest = wd.Decrypt(decdata, key);
+            
+        }
+    }
+}
